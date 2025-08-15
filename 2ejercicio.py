80f7ff1baff0a234370d33716523177cc64a352f
nchantre <nelsonfabiancs@hotmail.com>
2025-08-14 19:11:17 -0500

    ejercicios realizado 2 .3 14-08-2025

---
+++ 2ejercicio.py
@@ -0,0 +1,5 @@
+edad = int(input("Introduce tu edad: "))
+if edad < 18:
+    print("Eres menor de edad.")    
+else:
+    print("Eres mayor de edad.")
---
+++ 3-ejercicio.py
@@ -0,0 +1,8 @@
+# Programa para validar si una persona es mayor o menor de edad
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
